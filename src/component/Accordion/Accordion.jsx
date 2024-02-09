8425f97fc5dca9e128394159be7050d8d0e64702
Shraban Chakma <shrabanchakma328@gmail.com>
2024-02-10 04:53:33 +0600

    added handleMultiSelection function

--- src/component/Accordion/Accordion.jsx
+++ src/component/Accordion/Accordion.jsx
@@ -3,17 +3,39 @@ import data from "./data";
 import "./Accordion.css";
 export default function Accordion() {
   const [selected, setSelected] = useState(null);
+  const [enableMultiSelection, setEnableMultiSelection] = useState(false);
+  const [multiple, setMultiple] = useState([]);
 
   function handleSingleSelection(getCurrentId) {
     setSelected(getCurrentId === selected ? null : getCurrentId);
   }
+
+  function handleMultiSelection(getCurrentId) {
+    let cpyMultiple = [...multiple];
+    const findIndexOfCurrentId = cpyMultiple.indexOf(getCurrentId);
+
+    if (findIndexOfCurrentId === -1) cpyMultiple.push(getCurrentId);
+    else cpyMultiple.splice(findIndexOfCurrentId, 1);
+
+    setMultiple(cpyMultiple);
+  }
+
+  console.log(multiple);
+
   return (
     <div className="wrapper">
+      <button onClick={() => setEnableMultiSelection(!enableMultiSelection)}>
+        Enable Multi Selection
+      </button>
       {data && data.length > 0 ? (
         data.map((dataItem) => (
           <div className="item" key={dataItem.id}>
             <div
-              onClick={() => handleSingleSelection(dataItem.id)}
+              onClick={
+                enableMultiSelection
+                  ? () => handleMultiSelection(dataItem.id)
+                  : () => handleSingleSelection(dataItem.id)
+              }
               className="title"
             >
               <h3>{dataItem.question}</h3>
