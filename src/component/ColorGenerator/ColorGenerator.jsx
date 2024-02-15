b8eb152272339f9d002ab94f30482ea9f77f8126
Shraban Chakma <shrabanchakma328@gmail.com>
2024-02-16 00:21:33 +0600

    hex color generator function updated

--- src/component/ColorGenerator/ColorGenerator.jsx
+++ src/component/ColorGenerator/ColorGenerator.jsx
@@ -2,9 +2,45 @@ import { useState } from "react";
 
 export default function ColorGenerator() {
   const [color, setColor] = useState("#000000");
-  function generateColor() {
-    const newColor = "#" + Math.floor(Math.random() * (256 * 3));
+  const [colorType, setColorType] = useState("");
+
+  function randomColorUtility(length) {
+    return Math.floor(Math.random() * length);
   }
+  function generateHexColor() {
+    const hexValues = [
+      "0",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "A",
+      "B",
+      "C",
+      "D",
+      "E",
+      "F",
+    ];
+    let hexColor = "#";
+    while (hexColor.length < 7)
+      hexColor += hexValues[randomColorUtility(hexValues.length)];
+    setColor(hexColor);
+  }
+
+  function hexToRGB(hex) {
+    hex = hex.replace("#", "");
+    const r = parseInt(hex.substring(0, 2), 16);
+    const g = parseInt(hex.substring(2, 4), 16);
+    const b = parseInt(hex.substring(4, 6), 16);
+    console.log(r, g, b);
+    setColorType(`rgb(${r},${g},${b})`);
+  }
+
   return (
     <div
       style={{
@@ -17,7 +53,15 @@ export default function ColorGenerator() {
         justifyContent: "center",
       }}
     >
-      <div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <h1>{colorType}</h1>
         <button
           style={{
             padding: "1rem 2rem",
@@ -25,9 +69,20 @@ export default function ColorGenerator() {
             borderRadius: "1.5rem",
             fontWeight: "600",
           }}
-          onClick={generateColor}
+          onClick={generateHexColor}
         >
-          Generate Color
+          Generate Hex Color
+        </button>
+        <button
+          style={{
+            padding: "1rem 2rem",
+            border: "none",
+            borderRadius: "1.5rem",
+            fontWeight: "600",
+          }}
+          onClick={() => hexToRGB(color)}
+        >
+          convert to rgb
         </button>
       </div>
     </div>
